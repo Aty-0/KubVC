c0fffd787c73ae30165d360db80581e1665630f1
A_T <danich-anton-live@yandex.ru>
2026-05-11 03:23:36 +0300

    use ranges for container helper functions

--- src/container.h
+++ src/container.h
@@ -1,26 +1,17 @@
 #pragma once
-#include <string>
 #include <optional>
 #include <initializer_list>
+#include <algorithm> 
 
 namespace kubvc::utility::container {  
     template<typename Key, typename T>
     inline static constexpr std::optional<T> get(const std::initializer_list<std::pair<Key, T>>& data, Key key) {
-        const auto result = std::find_if(data.begin(), data.end(), [key](const auto& it) {
-            return it.first == key;
-        });
-        if (result == data.end()) {
-            return std::nullopt;
-        }
-        return result->second;
+        const auto& result = std::ranges::find_if(data, [key](const auto& it) { return it.first == key; });
+        return result == data.end() ? std::nullopt : result->second;
     }
     
     template<typename Key, typename T>
     inline static constexpr bool find(const std::initializer_list<std::pair<Key, T>>& data, Key key) {
-        return std::find_if(data.begin(), data.end(), [key](const auto& it) {
-            return it.first == key;
-        }) != data.end();
+        return std::ranges::find_if(data, [key](const auto& it) { return it.first == key; }) != data.end();
     }
-
-    
 }
