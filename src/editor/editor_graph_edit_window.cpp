3ed2d6e12bbd5a2d79fe7c7b405e7359f856cc01
A_T <danich-anton-live@yandex.ru>
2026-05-14 22:02:32 +0300

    Remove and simplify ast debug panel

--- src/editor/editor_graph_edit_window.cpp
+++ src/editor/editor_graph_edit_window.cpp
@@ -17,220 +17,95 @@ namespace kubvc::editor {
     }
 
 #if defined(KUB_IS_DEBUG) || defined(SHOW_DEBUG_TOOLS_ON_RELEASE) 
-    static void showTreeList(std::shared_ptr<kubvc::algorithm::INode> start) {
+    static void showTreeList(const std::shared_ptr<kubvc::algorithm::INode> start) {
+        static constexpr auto TREE_NODE_FLAGS = ImGuiTreeNodeFlags_DefaultOpen;
+
         // We are reached the end of tree 
         if (start == nullptr) {
             return;
         }
 
-        auto type = start->getType();
-        auto nodeName = std::string();
-
-        static const auto flags = ImGuiTreeNodeFlags_DefaultOpen;
-        switch (type)
-        {
-            case kubvc::algorithm::NodeTypes::Root: {
-                const auto node = algorithm::castToNodePtr<algorithm::NodeTypes::Root>(start);
-                nodeName = "Root id:" + std::to_string(node->getId());
-                if (ImGui::TreeNodeEx(nodeName.c_str(), flags)) {
-                    showTreeList(node->child);
+        const auto type = start->getType();
+        const auto nodeName = algorithm::getNodeName(start->getType());
+        switch (type) {
+            case algorithm::NodeTypes::Root: {
+                const auto node = castToNodePtr<algorithm::NodeTypes::Root>(start);
+                if (ImGui::TreeNodeEx(nodeName.data(), TREE_NODE_FLAGS)) {
+                    if (!node->child) {
+                        ImGui::Text("Child is empty");
+                    } else {
+                        showTreeList(node->child);
+                    }
                     ImGui::TreePop();  
                 }  
                 break;
             }
-            case kubvc::algorithm::NodeTypes::Number: {
-                const auto node = algorithm::castToNodePtr<algorithm::NodeTypes::Number>(start);         
-                nodeName = std::to_string(node->getValue()) + " id:" + std::to_string(node->getId());
-
-                if (ImGui::TreeNodeEx(nodeName.c_str(), flags)) {
-                    ImGui::TreePop();  
-                }  
-                break;            
-            }
-            case kubvc::algorithm::NodeTypes::Operator: {
-                const auto node = algorithm::castToNodePtr<algorithm::NodeTypes::Operator>(start);         
-                nodeName = std::string(1, node->operation) + " id:" + std::to_string(node->getId()); 
-                if (ImGui::TreeNodeEx(nodeName.c_str(), flags)) {
+            case algorithm::NodeTypes::Operator: {
+                const auto node = castToNodePtr<algorithm::NodeTypes::Operator>(start);  
+                if (ImGui::TreeNodeEx(nodeName.data(), TREE_NODE_FLAGS)) {
                     ImGui::Text("Left");
-                    showTreeList(node->left);            
+                    // Recursively clear left and right subtrees
+                    if (node->left != nullptr) {
+                        showTreeList(node->left);
+                    }
+
                     ImGui::Text("Right");
-                    showTreeList(node->right);
+                    if (node->right != nullptr) {
+                        showTreeList(node->right);
+                    }
                     ImGui::TreePop();  
                 }  
+
+
                 break;    
             }
-            case kubvc::algorithm::NodeTypes::Variable: {
-                const auto node = algorithm::castToNodePtr<algorithm::NodeTypes::Variable>(start);         
-                nodeName = std::string(1, node->getValue()) + " id:" + std::to_string(node->getId()); 
-                if (ImGui::TreeNodeEx(nodeName.c_str(), flags)) {
+            case algorithm::NodeTypes::UnaryOperator: {
+                const auto node = castToNodePtr<algorithm::NodeTypes::UnaryOperator>(start);   
+
+                if (ImGui::TreeNodeEx(nodeName.data(), TREE_NODE_FLAGS)) {
+                    if (node->child != nullptr) {
+                        showTreeList(node->child);
+                    }
                     ImGui::TreePop();  
-                }  
+                } 
+
+                break;     
+            }
+            case algorithm::NodeTypes::Function: {
+                const auto node = castToNodePtr<algorithm::NodeTypes::Function>(start);    
+                
+                ImGui::Text("%s", node->name.c_str());
+                if (ImGui::TreeNodeEx(nodeName.data(), TREE_NODE_FLAGS)) {
+                    if (node->argument != nullptr) {
+                        showTreeList(node->argument);
+                    }
+                    ImGui::TreePop();  
+                } 
+
                 break;
             }
-            case kubvc::algorithm::NodeTypes::UnaryOperator: {
-                const auto node = algorithm::castToNodePtr<algorithm::NodeTypes::UnaryOperator>(start);         
-                nodeName = std::string(1, node->operation) + " id:" + std::to_string(node->getId()); 
-                if (ImGui::TreeNodeEx(nodeName.c_str(), flags)) {
-                    showTreeList(node->child);     
-                    ImGui::TreePop();  
-                }  
+            case algorithm::NodeTypes::Number: {
+                const auto node = castToNodePtr<algorithm::NodeTypes::Number>(start);    
+                ImGui::Text("%f", node->getValue());
                 break;
             }
-            case kubvc::algorithm::NodeTypes::Function: {
-                const auto node = algorithm::castToNodePtr<algorithm::NodeTypes::Function>(start);         
-                nodeName = node->name + " id:"  +std::to_string(node->getId()); 
-                if (ImGui::TreeNodeEx(nodeName.c_str(), flags)) {
-                    ImGui::TreePop();  
-                }  
+            case algorithm::NodeTypes::Variable: {
+                const auto node = castToNodePtr<algorithm::NodeTypes::Variable>(start);    
+                ImGui::Text("%c", node->getValue());
+                ImGui::SameLine();
+                ImGui::Text("isParameter: %i", node->isParameter);
+                ImGui::Text("value: %f", node->parameter);
                 break;
             }
-            case kubvc::algorithm::NodeTypes::Invalid: {
-                const auto node = algorithm::castToNodePtr<algorithm::NodeTypes::Invalid>(start);         
-                nodeName = node->name + " id:" + std::to_string(node->getId());   
-                ImGui::TextColored(ImVec4(255, 0, 0, 255), "Invalid");
+            case algorithm::NodeTypes::Invalid: {
+                ImGui::Text("invalid, id:%d", start->getId());
                 break;
             }
             default:
-                ImGui::TextColored(ImVec4(255, 0, 0, 255), "UNK");
-                break;
-        }
-    }
-
-    static constexpr auto MOVE = 100.0f;
-
-    static void showTree(const std::shared_ptr<kubvc::algorithm::INode>& start,
-        ImDrawList* drawList, ImVec2 origin, ImVec2 panOffset,
-        ImVec2 pos, bool hasParent, float spacing)
-    {
-        if (start == nullptr) {
-            return;
-        } 
-        
-        auto& style = ImGui::GetStyle();
-
-        const auto lineColor = style.Colors[ImGuiCol_::ImGuiCol_PlotLines] * 100;        
-        const auto lineColor32 = IM_COL32(lineColor.x, lineColor.y, lineColor.z, 255);
-        const auto textColor = style.Colors[ImGuiCol_::ImGuiCol_Text] * 100;   
-        const auto textColor32 = IM_COL32(textColor.x, textColor.y, textColor.z, 255);
-
-        auto nodePos = ImVec2(origin.x + panOffset.x + pos.x, origin.y + panOffset.y + pos.y);
-        std::string label;
-        switch (start->getType()) {
-            case kubvc::algorithm::NodeTypes::Root: {
-                const auto node = algorithm::castToNodePtr<algorithm::NodeTypes::Root>(start);   
-                label = "#" + std::to_string(node->getId());
-                drawList->AddText(nodePos, textColor32, label.c_str());
-
-                if (node->child) {
-                    hasParent = true;
-                    auto childPos = ImVec2(pos.x, pos.y + spacing);
-                    showTree(node->child, drawList, origin, panOffset, childPos, true, spacing);
-                    auto childAbs = ImVec2(origin.x + panOffset.x + childPos.x, origin.y + panOffset.y + childPos.y);
-                    drawList->AddLine(nodePos, childAbs, lineColor32, 2.0f);
-                }
-                break;
-            }
-            case kubvc::algorithm::NodeTypes::Number: {
-                const auto node = algorithm::castToNodePtr<algorithm::NodeTypes::Number>(start);   
-                label = std::to_string(node->getValue()) + " #" + std::to_string(node->getId());
-                drawList->AddText(nodePos, textColor32, label.c_str());
-                break;
-            }
-            case kubvc::algorithm::NodeTypes::Variable: {
-                const auto node = algorithm::castToNodePtr<algorithm::NodeTypes::Variable>(start);   
-                label = std::string(1, node->getValue()) + " #" + std::to_string(node->getId());
-                drawList->AddText(nodePos, textColor32, label.c_str());
-                break;
-            }
-            case kubvc::algorithm::NodeTypes::Operator: {
-                const auto node = algorithm::castToNodePtr<algorithm::NodeTypes::Operator>(start);   
-                label = std::string(1, node->operation) + " #" + std::to_string(node->getId());
-                drawList->AddText(nodePos, textColor32, label.c_str());
-
-                if (node->left) {
-                    auto leftPos = ImVec2(pos.x - spacing * 2, pos.y + spacing);
-                    showTree(node->left, drawList, origin, panOffset, leftPos, true, spacing);
-                    auto leftAbs = ImVec2(origin.x + panOffset.x + leftPos.x, origin.y + panOffset.y + leftPos.y);
-                    drawList->AddLine(nodePos, leftAbs, lineColor32, 2.0f);
-                }
-
-                if (node->right) {
-                    auto rightPos = ImVec2(pos.x + spacing * 2, pos.y + spacing);
-                    showTree(node->right, drawList, origin, panOffset, rightPos, true, spacing);
-                    auto rightAbs = ImVec2(origin.x + panOffset.x + rightPos.x, origin.y + panOffset.y + rightPos.y);
-                    drawList->AddLine(nodePos, rightAbs, lineColor32, 2.0f);
-                }
-                break;
-            }
-            case kubvc::algorithm::NodeTypes::UnaryOperator: {
-                const auto node = algorithm::castToNodePtr<algorithm::NodeTypes::UnaryOperator>(start);   
-                label = std::string(1, node->operation) + " #" + std::to_string(node->getId());
-                drawList->AddText(nodePos, textColor32, label.c_str());
-
-                if (node->child) {
-                    auto childPos = ImVec2(pos.x, pos.y + spacing);
-                    showTree(node->child, drawList, origin, panOffset, childPos, true, spacing);
-                    auto childAbs = ImVec2(origin.x + panOffset.x + childPos.x, origin.y + panOffset.y + childPos.y);
-                    drawList->AddLine(nodePos, childAbs, lineColor32, 2.0f);
-                }
-                break;
-            }
-            case kubvc::algorithm::NodeTypes::Function: {
-                const auto node = algorithm::castToNodePtr<algorithm::NodeTypes::Function>(start);   
-                label = node->name + " #" + std::to_string(node->getId());
-                drawList->AddText(nodePos, textColor32, label.c_str());
-
-                if (node->argument) {
-                    auto argPos = ImVec2(pos.x, pos.y + spacing);
-                    showTree(node->argument, drawList, origin, panOffset, argPos, true, spacing);
-                    auto argAbs = ImVec2(origin.x + panOffset.x + argPos.x, origin.y + panOffset.y + argPos.y);
-                    drawList->AddLine(nodePos, argAbs, lineColor32, 2.0f);
-                }
-                break;
-            }
-            case kubvc::algorithm::NodeTypes::Invalid: {
-                const auto node = algorithm::castToNodePtr<algorithm::NodeTypes::Invalid>(start);   
-                label = node->name + " #" + std::to_string(node->getId());
-                drawList->AddText(nodePos, IM_COL32(0, 255, 0, 255), label.c_str());
-                break;
-            }
-            default:
-                drawList->AddText(nodePos, IM_COL32(255, 0, 0, 255), "UNK");
+                KUB_ASSERT(true, "Unknown type");
                 break;
         }
 
-        if (hasParent) {
-            drawList->AddCircleFilled(nodePos, 3.0f, IM_COL32(255, 0, 0, 255));
-        }
-    }
-
-    static void showTreeVisual(const kubvc::algorithm::ASTree& tree) {            
-        static constexpr auto childFlags = ImGuiChildFlags_::ImGuiChildFlags_Borders; 
-        static auto dragOffset = ImVec2(0,0);
-        //static auto width = 0.65f;
-        const auto origin = ImGui::GetCursorScreenPos(); // Top-left of the child window
-        const auto drawList = ImGui::GetWindowDrawList();
-        // TODO: Soo, how we can implement zoom
-        //ImGui::SliderFloat("Width##TreeChildWindow", &width, 0.1f, 2.0f);
-
-        if (ImGui::BeginChild("TreeChildWindow", ImVec2(0,0), childFlags, ImGuiWindowFlags_NoInputs)) { 
-            auto min = ImGui::GetWindowPos();
-            auto max = ImVec2(min.x + ImGui::GetWindowWidth(), min.y + ImGui::GetWindowHeight());
-            drawList->PushClipRect(min, max, true);
-            //ImGui::PushItemWidth(ImGui::GetWindowWidth() * width);
-            ImGui::Text("drag %f %f", dragOffset.x, dragOffset.y);
-            const auto root = const_cast<kubvc::algorithm::ASTree&>(tree).getRoot();
-            showTree(algorithm::castToINodePtr<algorithm::NodeTypes::Root>(root), drawList, origin, dragOffset, ImVec2(0, 0), false, MOVE);
-            //ImGui::PopItemWidth();
-            drawList->PopClipRect();
-        }   
-
-        auto io = ImGui::GetIO();
-        if (ImGui::IsMouseDragging(ImGuiMouseButton_::ImGuiMouseButton_Right)) {
-            dragOffset.x += io.MouseDelta.x;
-            dragOffset.y += io.MouseDelta.y;
-        }
-        ImGui::EndChild();
     }
 
     static void drawDebugAST() {
@@ -239,15 +114,9 @@ namespace kubvc::editor {
             if (selected != nullptr) {
                 ImGui::Text("Current tree is %s", selected->getTextBuffer().getBuffer().data());   
 
-                static bool listStyleTree = false;
-                ImGui::Checkbox("List style for tree", &listStyleTree);
-                const auto tree = selected->getExpression().getTree();
-                if (listStyleTree) {
-                    showTreeList(tree.getRoot());
-                }
-                else {
-                    showTreeVisual(tree);
-                }
+                auto& expression = selected->getExpression();
+                auto& tree = expression.getTree();
+                showTreeList(tree.getRoot());
             }
             else {
                 ImGui::Text("No currently selected tree");
@@ -355,26 +224,6 @@ namespace kubvc::editor {
             const auto points = selected->getExpression().getPlotBuffer();
             ImGui::Text("Count: %zu", points.size());
             if (ImGui::CollapsingHeader("Points")) {
-                if (ImGui::Button("Dump points to file")) {
-                    std::ofstream filex;
-                    std::ofstream filey;
-                    std::ofstream filepoints;
-                    filex.open("dump_x.txt", std::ios_base::out);
-                    filey.open("dump_y.txt", std::ios_base::out);
-                    filepoints.open("filepoints.txt", std::ios_base::out);
-                    
-                    for (auto point : points) {
-                        filex << point.x << "\n";
-                        filey << point.y << "\n";
-                        filepoints << "(" << point.x << "," << point.y  << ")" << ",";
-                        //KUB_DEBUG("[Dump] x:%f y:%f", point.x, point.y);
-                    }
-
-                    filex.close();
-                    filey.close();
-                    filepoints.close();
-                }
-                ImGui::Separator();
                 for (auto point : selected->getExpression().getPlotBuffer()) {
                     if (glm::isnan(point.x) || glm::isnan(point.y)) {
                         ImGui::TextColored(ImVec4(1,0,0,1), "x:%f y:%f", point.x, point.y);
